58f3145548625d5fa32abda7ef73e6fec2d92760
MrNubi <99630193+MrNubi@users.noreply.github.com>
2022-03-11 14:40:57 +0900

    Initial commit

---
+++ app/src/main/java/com/beyond/mytimekeeper_20220311/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.beyond.mytimekeeper_20220311
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.beyond.mytimekeeper_20220311.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+
+    }
+}
